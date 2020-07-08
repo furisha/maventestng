c12a6896e2144454f0da92fc5f9dea8b418ac6a1
furisha <vladanfurman@gmail.com>
2020-07-08 23:11:39 +0200

    This is a new version

---
+++ maventestng/src/test/java/com/furman/MyTestNGClass.java
@@ -0,0 +1,27 @@
+package com.furman;
+
+import org.testng.annotations.Test;
+
+public class MyTestNGClass {
+
+    @Test
+    public  void setup1(){
+        System.out.println("Walcome1");
+    }
+
+    @Test
+    public  void setup2(){
+        System.out.println("Walcome2");
+    }
+
+    @Test
+    public  void setup3(){
+        System.out.println("Walcome3");
+    }
+
+    @Test
+    public  void setup4(){
+        System.out.println("Walcome4");
+    }
+
+}
